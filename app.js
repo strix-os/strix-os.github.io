a818f167fbf50e611c7359a39645cc992f7a24ea
strix-os <strix.hacking.os@gmail.com>
2024-05-21 17:26:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // JavaScript para la página web (en este caso no se necesita ninguna interacción)
+window.addEventListener('scroll', function() {
+    var scrollPos = window.scrollY;
+    var background = document.querySelector('.background');
+    
+    // Modifica la opacidad del fondo en función del desplazamiento
+    background.style.opacity = (scrollPos / window.innerHeight).toFixed(2);
+});
